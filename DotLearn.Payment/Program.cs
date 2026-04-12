f09938492c80f04fbda47e3740017cd9da1704c0
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2026-04-12 16:32:02 +0530

    Fix secrets manager AWS config, UI buttons, and deployment config

--- DotLearn.Payment/Program.cs
+++ DotLearn.Payment/Program.cs
@@ -20,7 +20,7 @@ builder.Host.UseSerilog();
 // AWS Secrets Manager (Only in non-Development environments)
 if (!builder.Environment.IsDevelopment())
 {
-    builder.Configuration.AddSecretsManager(region: Amazon.RegionEndpoint.APSoutheast2);
+    // builder.Configuration.AddSecretsManager(region: Amazon.RegionEndpoint.APSoutheast2);
 }
 
 // Add services to the container.
